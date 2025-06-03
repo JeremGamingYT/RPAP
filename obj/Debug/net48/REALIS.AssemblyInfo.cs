bbddae8307f580caf5fb34bf6adf4df981c21169
JeremGaming <98130953+JeremGamingYT@users.noreply.github.com>
2025-06-03 18:02:41 -0400

    Adding ScriptHookVDotNet V3

--- obj/Debug/net48/REALIS.AssemblyInfo.cs
+++ obj/Debug/net48/REALIS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REALIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854c4671aa67c8e57de9d80e863913cb4be3f55e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48259113ae6f81fa78b2e20bf0c78dd6c9601bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("REALIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REALIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
